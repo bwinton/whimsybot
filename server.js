15e660c15e915ca049fee1ac30723d171320be2c
Blake Winton <bwinton@latte.ca>
2014-11-04 14:34:59 -0500

    Make it say better stuff.

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-var etherpad = require('./lib/etherpad'),
-  irc = require('irc');
+#!/usr/bin/env node
+
+var chalk = require('chalk');
+var etherpad = require('./lib/etherpad');
+var irc = require('irc');
 
 var getWhimsy = etherpad.load('urlbar-sayings');
 
 if (!module.parent) {
-  var bot = new irc.Client('irc.mozilla.org', 'whimsy', {
-    channels: ['#whimsybot'],
+  var bot = new irc.Client('irc.mozilla.org', 'whimsybot', {
+    channels: ['#whimsy'],
     secure: true,
     port: 6697
   });
 
   bot.addListener('message', function(from, to, message) {
-    if (/whimsybot/.test) {
-      bot.say(to, getWhimsy());
+    var saying = getWhimsy();
+    if (/whimsybot/.test(message)) {
+      console.log('In '  + chalk.red.bold(to) + ' Telling ' + chalk.blue.bold(from) +
+        chalk.yellow(' "' + saying + '"') + '.');
+      bot.say(to, saying);
+    } else if (to === 'whimsybot') {
+      console.log('Privately telling ' + chalk.blue.bold(from) +
+        chalk.yellow(' "' + saying + '"') + '.');
+      bot.say(from, saying);
     }
   });
 }
