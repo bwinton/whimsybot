e1b569a3221fc4082191624518021a18228189c4
Blake Winton <bwinton@latte.ca>
2015-02-22 15:12:58 -0500

    Refactor the message handling out to a function, and add an error handler.

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ var irc = require('irc');
 
 var getWhimsy = etherpad.load('urlbar-sayings');
 
+var handleMessage = function (from, to, message) {
+  if (!message) {
+    message = to;
+    to = from;
+  }
+  var reply;
+  var logMessage;
+  if (/http.*\.gif/.test(message)) {
+    logMessage = 'Got ';
+    if (from === to) {
+      logMessage += 'private ';
+    }
+    logMessage += 'gif from ' + chalk.blue.bold(from) + ': ' +
+      message.replace(/^whimsybot:/, '').trim();
+    reply = 'Gif submitted.  Thanks!  :)';
+  } else {
+    reply = getWhimsy();
+    if (from !== to) {
+      logMessage = 'In '  + chalk.red.bold(to);
+    } else {
+      logMessage = 'Privately';
+    }
+    logMessage += ' telling ' + chalk.blue.bold(from) +
+      chalk.yellow(' "' + reply + '"') + '.';
+  }
+  console.log(logMessage);
+  bot.say(to, reply);
+}
+
 if (!module.parent) {
   var bot = new irc.Client('irc.mozilla.org', 'whimsybot', {
     channels: ['#whimsy'],
@@ -14,15 +43,14 @@ if (!module.parent) {
   });
 
   bot.addListener('message', function(from, to, message) {
-    var saying = getWhimsy();
-    if (/whimsybot/.test(message)) {
-      console.log('In '  + chalk.red.bold(to) + ' Telling ' + chalk.blue.bold(from) +
-        chalk.yellow(' "' + saying + '"') + '.');
-      bot.say(to, saying);
-    } else if (to === 'whimsybot') {
-      console.log('Privately telling ' + chalk.blue.bold(from) +
-        chalk.yellow(' "' + saying + '"') + '.');
-      bot.say(from, saying);
+    if (to === 'whimsybot' && from !== 'whimsybot') {
+      handleMessage(from, message);
+    } else if (/whimsybot/.test(message)) {
+      handleMessage(from, to, message);
     }
+  });
+
+  bot.addListener('error', function(message) {
+    console.log('error: ', message);
   });
 }
